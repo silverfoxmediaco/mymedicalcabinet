e892c5d7bf2fbdde2d84a19088c0dc3ade3cb37e
James McEwen <james@silverfoxmedia.co>
2025-04-17 12:41:22 -0500

    Update login.js

--- login.js
+++ login.js
@@ -1,29 +1,31 @@
-// public / login.js
+// public/login.js
 
-document.getElementById("login-form").addEventListener("submit", async (e) => {e.preventDefault();
-    const username = document.getElementById("username").value.trim();
-    const password = document.getElementById("password").value.trim();
-    const errorMsg = document.getElementById("login-error");
+document.getElementById("login-form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const username = document.getElementById("username").value.trim();
+  const password = document.getElementById("password").value.trim();
+  const errorMsg = document.getElementById("login-error");
 
-    try {
-        const res = await fetch("https://mymedicalcabinet.onrender.com/api/login", {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ username, password})
-        });
+  try {
+    const res = await fetch("https://mymedicalcabinet.onrender.com/api/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ username, password })
+    });
 
-        const data = await res.json();
+    const data = await res.json();
 
-       if (data.role === "physician") {
+    if (res.ok) {
+      if (data.role === "physician") {
         window.location.href = "/physician-profile.html";
-        } else {
+      } else {
         window.location.href = "/mydashboard.html";
+      }
+    } else {
+      errorMsg.textContent = data.message || "Login failed.";
     }
-        } else {
-            errorMsg.textContent = data.message || "Login failed.";
-        }
-    } catch (err) {
-        console.error("Login error:", err);
-        errorMsg.textContent = "Something went wrong, check password and username.";
-    }
-})
+  } catch (err) {
+    console.error("Login error:", err);
+    errorMsg.textContent = "Something went wrong, check password and username.";
+  }
+});
